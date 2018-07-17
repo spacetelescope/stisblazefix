40e287bcd8c888f67a36b2eb549473612f97265c
Sean Lockwood <sean-lockwood@users.noreply.github.com>
2018-07-17 11:43:48 -0400

    Put sphinx configuration path back in an attempt to fix the docs

--- doc/conf.py
+++ doc/conf.py
@@ -20,7 +20,7 @@
 import alabaster
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../stisblazefix'))
 
 
 # -- General configuration ------------------------------------------------
